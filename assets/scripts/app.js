df007f6690dda9d51c3f36ef05cbda6523b501db
logicmyth <staley77@protonmail.com>
2016-08-02 10:42:08 -0400

    Header, Nav bar, Styling
    Add nav bar, header, styled header font and imgs for background of nav and modal.

---
+++ assets/scripts/app.js
@@ -0,0 +1,5 @@
+'use strict';
+module.exports = {
+   host: 'http://localhost:3000',
+  //host: 'https://qtunes-server.herokuapp.com',
+};
---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,66 @@
+'use strict';
+//
+const getFormFields = require('../../../lib/get-form-fields');
+
+const api = require('./api');
+const ui = require('./ui');
+
+
+const onSignUp = (event) => {
+ event.preventDefault();
+ let data = getFormFields(event.target);
+ api.signUp(data)
+ .done(ui.success)
+ .fail(ui.failure);
+};
+
+const onSignIn = (event) => {
+ event.preventDefault();
+ //api.getUserIDPlaylist();
+ let data = getFormFields(event.target);
+ api.signIn(data)
+ .done(ui.signInSuccess)
+ .fail(ui.failure);
+};
+
+const onSignOut = (event) => {
+ event.preventDefault();
+ api.signOut()
+ .done(ui.signOutSuccess)
+ .fail(ui.failure);
+};
+
+const onChangePassword = (event) => {
+ event.preventDefault();
+ let data = getFormFields(event.target);
+ api.changePassword(data)
+ .done(ui.success)
+ .fail(ui.failure);
+};
+
+const onNavSignUp = () => {
+  console.log("We're inside onNavSignUp");
+   $('#open-sign-up').modal('show');
+};
+
+const onNavSignIn = () => {
+   $('#open-sign-in').modal('show');
+};
+
+const onNavOptions = () => {
+   $('#open-options').modal('show');
+};
+
+const addHandlers = () => {
+ $('#sign-up').on('submit', onSignUp);
+ $('#sign-in').on('submit', onSignIn);
+ $('#sign-out').on('submit', onSignOut);
+ $('#change-password').on('submit', onChangePassword);
+ $('#nav-sign-up').on('click', onNavSignUp);
+ $('#nav-sign-in').on('click', onNavSignIn);
+ $('#nav-options').on('click', onNavOptions);
+};
+
+module.exports = {
+ addHandlers,
+};
